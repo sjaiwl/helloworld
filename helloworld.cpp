9eedf36edbae281ed1bd772bdad7b47e43ad0dd6
王林 <761034053@qq.com>
2020-05-24 13:41:05 +0800

    添加注释

--- helloworld.cpp
+++ helloworld.cpp
@@ -8,6 +8,7 @@ int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
+    //TODO：确认实现方式？？
     for (const string& word : msg)
     {
         cout << word << " ";
